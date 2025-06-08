d1835e081c0b5622f779d7ce3961a54af6251295
mlee0323 <musicbox0323@gmail.com>
2025-06-08 17:11:28 +0900

    학습 파일 수정

--- models/stocks/lg_electronics.py
+++ models/stocks/lg_electronics.py
@@ -6,6 +6,9 @@ from datetime import datetime, timedelta
 import logging
 from typing import Dict, Any, Optional, Tuple, List
 
+# GPU 사용 비활성화
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 from models.base.price_predict_model import BasePricePredictModel, setup_gpu, enhanced_weighted_time_mse
 from database.database import DatabaseManager
 
@@ -35,6 +38,9 @@ class LGElectronicsModel(BasePricePredictModel):
         self.models = []  # 앙상블 모델 리스트
         self.num_models = 3  # 앙상블 모델 수
         
+        # CPU 사용 설정
+        tf.config.set_visible_devices([], 'GPU')
+
     def load_data(self) -> pd.DataFrame:
         """LG전자 주가 데이터 로드"""
         try:
